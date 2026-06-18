e183e034a09ae7232d8de2f75e04e38a27ccac0f
Robert Gabriel Elias Beltrão <robert.beltrao@upe.br>
2026-06-18 00:04:10 -0300

    Resolução dos desafios

--- challenges/lowest_common_ancestor.py
+++ challenges/lowest_common_ancestor.py
@@ -6,4 +6,14 @@ def lowest_common_ancestor(
     value1: int,
     value2: int,
 ) -> int:
-    raise NotImplementedError
+
+    while root is not None:
+
+        if value1 < root.value and value2 < root.value:
+            root = root.left
+
+        elif value1 > root.value and value2 > root.value:
+            root = root.right
+
+        else:
+            return root.value
--- challenges/tree_height.py
+++ challenges/tree_height.py
@@ -2,4 +2,11 @@ from data_structures.node import Node
 
 
 def tree_height(root: Node | None) -> int:
-    raise NotImplementedError
+
+    if root is None:
+        return -1
+
+    return 1 + max(
+        tree_height(root.left),
+        tree_height(root.right)
+    )
